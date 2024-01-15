a43f44e1aa0ea208388506bd66307b6e7dc1fed6
RobKaya <robkaya@icloud.com>
2024-01-16 00:45:16 +0100

    First commit

---
+++ src/main/java/com/wacoco/nowle/NowleApplication.java
@@ -0,0 +1,13 @@
+package com.wacoco.nowle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wacoco/nowle/NowleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wacoco.nowle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
