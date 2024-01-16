4946e2986a19be166c3e7470f4469f2b8274f8da
samyuktha215 <samyuktha.basam@gmail.com>
2024-01-16 10:27:41 +0100

    added service class

--- src/main/java/com/wacoco/nowle/Routes/CamelRoutes.java
+++ src/main/java/com/wacoco/nowle/Routes/CamelRoutes.java
@@ -9,12 +9,12 @@ public class CamelRoutes extends RouteBuilder {
     public void configure() throws Exception {
         from("direct:patent")
                 .to("http://localhost:3000/patent")
-                .log("Data retrieved from Camel controller")
-                .log("");
+                .log("Data retrieved from Patent server: ${body}")
+                .to("log:myLogger?level=INFO");
         from("direct:linkedin")
                 .to("http://localhost:3000/linkedin")
-                .log("Data retrieved from Camel controller")
-                .log("");
+                .log("Data retrieved from Linkedin server :${body}")
+                .to("log:myLogger?level=INFO");
 
     }
 }
--- src/main/java/com/wacoco/nowle/Service/CamelService.java
+++ src/main/java/com/wacoco/nowle/Service/CamelService.java
@@ -1,4 +1,24 @@
 package com.wacoco.nowle.Service;
 
+import com.wacoco.nowle.Controller.CamelController;
+import org.apache.camel.CamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CamelService {
+    private static final Logger logger= LoggerFactory.getLogger(CamelController.class);
+    private final CamelContext camelContext;
+
+    public CamelService(CamelContext camelContext) {
+        this.camelContext = camelContext;
+    }
+    public String patentRoute(){
+        return "";
+    }
+    public String linkedinRoute(){
+        return "";
+    }
+
 }
