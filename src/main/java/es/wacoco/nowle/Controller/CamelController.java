4372ba5d72d7639ddd2bd4d4b05c53c34e6c93ab
stykle <Maxamadabdulahist@outlook.com>
2024-01-16 13:34:53 +0100

    added dev requestmapping

--- src/main/java/es/wacoco/nowle/Controller/CamelController.java
+++ src/main/java/es/wacoco/nowle/Controller/CamelController.java
@@ -3,9 +3,11 @@ package es.wacoco.nowle.Controller;
 import es.wacoco.nowle.Service.CamelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/dev")
 public class CamelController {
     private final CamelService camelService;
 @Autowired
