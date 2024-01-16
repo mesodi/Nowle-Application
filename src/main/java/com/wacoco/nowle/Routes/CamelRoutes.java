4acc1f8eaf47396ede0759cf174e92450f86623c
samyuktha215 <samyuktha.basam@gmail.com>
2024-01-16 09:38:43 +0100

    added router package and service package

---
+++ src/main/java/com/wacoco/nowle/Routes/CamelRoutes.java
@@ -0,0 +1,10 @@
+package com.wacoco.nowle.Routes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class CamelRoutes extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
