2000dfe3a732b11af80dcf1c7679ce904008ec59
samyuktha215 <samyuktha.basam@gmail.com>
2024-01-16 11:28:49 +0100

    added some dependencies

--- src/main/java/com/wacoco/nowle/Service/CamelService.java
+++ src/main/java/com/wacoco/nowle/Service/CamelService.java
@@ -2,9 +2,13 @@ package com.wacoco.nowle.Service;
 
 import com.wacoco.nowle.Controller.CamelController;
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class CamelService {
@@ -15,10 +19,32 @@ public class CamelService {
         this.camelContext = camelContext;
     }
     public String patentRoute(){
-        return "";
+        try{
+            ProducerTemplate producerTemplate= camelContext.createProducerTemplate();
+            String response=producerTemplate.requestBody("direct:patent",null, String.class);
+          return "Camel route process initiated! Response from patent server:\n" + response +"\n:Date"+getFormattedTimestamp();
+        }catch (Exception e){
+            e.printStackTrace();
+            return "Error occurred in getting the patent data ";
+
+        }
+
     }
     public String linkedinRoute(){
-        return "";
+        try {
+            ProducerTemplate producerTemplate= camelContext.createProducerTemplate();
+            String response=producerTemplate.requestBody("direct:linkedin",null, String.class);
+            return "Camel route process initiated! Response from linkedin server:\n"+response+"\n:Date"+getFormattedTimestamp();
+        }catch (Exception e) {
+            e.printStackTrace();
+            return "Error occurred in getting the linkedin data ";
+        }
+    }
+    private String getFormattedTimestamp() {
+        // Get the current timestamp using a custom formatter without milliseconds
+        LocalDateTime timestamp = LocalDateTime.now();
+        DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return timestamp.format(customFormatter);
     }
 
 }
